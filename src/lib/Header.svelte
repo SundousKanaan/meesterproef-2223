8573c4f37c713e7e68643a69a0a99af0660e1ea2
DutchEllie <personal@quenten.nl>
2023-06-07 15:54:44 +0200

    Oi

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,377 +1,391 @@
 <script>
-	import { browser } from "$app/environment";
+	import { browser } from '$app/environment';
 
-    let active = false;
-    let loggedIn = false;
-    /**
+	let active = false;
+	let loggedIn = false;
+	/**
 	 * @type {string | null}
 	 */
-    let openItem = null; 
+	let openItem = null;
 
-    function logIn() {
-        loggedIn = true;
-    }
+	function logIn() {
+		loggedIn = true;
+	}
 
-    function logOut() {  
-        loggedIn = false;
-    }
+	function logOut() {
+		loggedIn = false;
+	}
 
-    /**
+	/**
 	 * @param {string | null} id
 	 */
-    function toggleOpen(id) {
-        openItem = openItem === id ? null : id;
-    }
+	function toggleOpen(id) {
+		openItem = openItem === id ? null : id;
+	}
 
-    // Idk, I think this is properly done? Idk if this is normal in Svelte, but it works.
-    // Done to fix an issue where the body would scroll when the menu is open.
-    $: if (browser) document.body.classList.toggle('body-no-scroll', active);
+	// Idk, I think this is properly done? Idk if this is normal in Svelte, but it works.
+	// Done to fix an issue where the body would scroll when the menu is open.
+	$: if (browser) document.body.classList.toggle('body-no-scroll', active);
 </script>
-
 
 <!-- <svelte:window on:resize={() => windowWidth = window.innerWidth} /> -->
 
 <header class={active ? 'active' : ''}>
-    <a href="#"><img src="/logo_tioc_yellow.svg" alt=""></a>
-    <!-- {#if menuShown} -->
-    <!-- A button with the class to hide the navigation. it must be readable for people with screenreaders by using aria-->
-    <button class="menu-hider" aria-label="hide navigation" on:click={() => active = !active}></button>
-    <nav>
-        <ul>
-            <li class={openItem === 'item1' ? 'open' : ''} on:click={() => toggleOpen('item1')}>
-                <a href="#">Home</a>
-            </li>
-            <li class={openItem === 'item2' ? 'open' : ''} on:click={() => toggleOpen('item2')}>
-                <p>Learn</p>
-                <div>
-                    <a href="#">Articles</a>
-                </div>
-            </li>
-            <li class={openItem === 'item3' ? 'open' : ''} on:click={() => toggleOpen('item3')}>
-                <p>Discover</p>
-                <div>
-                    <a href="#">Creatives</a>
-                    <a href="#">Inspiration</a>
-                </div>
-            </li>
-            <li class={openItem === 'item4' ? 'open' : ''} on:click={() => toggleOpen('item4')}>
-                <p>Grow</p>
-                <div>
-                    <a href="#">Challenges</a>
-                    <a href="#">Workshops</a>
-                </div>
-            </li>
-            <li class={openItem === 'item5' ? 'open' : ''} on:click={() => toggleOpen('item5')}>
-                <p>About TIOC</p>
-                <div>
-                    <a href="#">What we do</a>
-                    <a href="#">Team TIOC</a>
-                    <a href="#">Partners</a>
-                    <a href="#">Sponsors</a>
-                    <a href="#">Contact</a>
-                </div>
-                </li>
-                <li class={openItem === 'item6' ? 'open' : ''} on:click={() => toggleOpen('item6')}>
-                    <p>Register</p>
-                    <div>
-                        <a href="#">Why join?</a>
-                        <a href="#">FAQ</a>
-                    </div>
-                </li>
-                {#if loggedIn}
-                <li>
-                    <img src="" alt="" class="avatar">
-                    <p>Naam hier</p>
-                    <div>
-                        <ul>
-                            <li><a href="#">Portfolio</a></li>
-                            <li><a href="#">My profile</a></li>
-                            <li><a href="#">Settings</a></li>
-                            <li><a href="#" on:click={logOut}>Sign out</a></li> <!-- add on:click event -->
-                        </ul>
-                    </div>
-                </li>
-            {:else}
-                <li>
-                    <p>Welkom gebruiker</p>
-                    <div>
-                        <ul>
-                            <li><a href="#" on:click|preventDefault={logIn}>Log in</a></li>
-                            <li><a href="#">Registreer</a></li>
-                        </ul>
-                    </div>
-                </li>
-            {/if}
-        </ul>
-    </nav>
-    <!-- {/if} -->
-    <button class="menu-button" on:click={() => active = !active}>
-        <svg width="2em" height="2em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#fff">
+	<a href="#"><img src="/logo_tioc_yellow.svg" alt="" /></a>
+	<!-- {#if menuShown} -->
+	<!-- A button with the class to hide the navigation. it must be readable for people with screenreaders by using aria-->
+	<button class="menu-hider" aria-label="hide navigation" on:click={() => (active = !active)} />
+	<nav>
+		<ul>
+			<li class={openItem === 'item1' ? 'open' : ''} on:click={() => toggleOpen('item1')}>
+				<a href="#">Home</a>
+			</li>
+			<li class={openItem === 'item2' ? 'open' : ''} on:click={() => toggleOpen('item2')}>
+				<p>Learn</p>
+				<div>
+					<a href="#">Articles</a>
+				</div>
+			</li>
+			<li class={openItem === 'item3' ? 'open' : ''} on:click={() => toggleOpen('item3')}>
+				<p>Discover</p>
+				<div>
+					<a href="#">Creatives</a>
+					<a href="#">Inspiration</a>
+				</div>
+			</li>
+			<li class={openItem === 'item4' ? 'open' : ''} on:click={() => toggleOpen('item4')}>
+				<p>Grow</p>
+				<div>
+					<a href="#">Challenges</a>
+					<a href="#">Workshops</a>
+				</div>
+			</li>
+			<li class={openItem === 'item5' ? 'open' : ''} on:click={() => toggleOpen('item5')}>
+				<p>About TIOC</p>
+				<div>
+					<a href="#">What we do</a>
+					<a href="#">Team TIOC</a>
+					<a href="#">Partners</a>
+					<a href="#">Sponsors</a>
+					<a href="#">Contact</a>
+				</div>
+			</li>
+			<li class={openItem === 'item6' ? 'open' : ''} on:click={() => toggleOpen('item6')}>
+				<p>Register</p>
+				<div>
+					<a href="#">Why join?</a>
+					<a href="#">FAQ</a>
+				</div>
+			</li>
+			{#if loggedIn}
+				<li>
+					<img src="" alt="" class="avatar" />
+					<p>Naam hier</p>
+					<div>
+						<ul>
+							<li><a href="#">Portfolio</a></li>
+							<li><a href="#">My profile</a></li>
+							<li><a href="#">Settings</a></li>
+							<li><a href="#" on:click={logOut}>Sign out</a></li>
+							<!-- add on:click event -->
+						</ul>
+					</div>
+				</li>
+			{:else}
+				<li>
+					<p>Welkom gebruiker</p>
+					<div>
+						<ul>
+							<li><a href="#" on:click|preventDefault={logIn}>Log in</a></li>
+							<li><a href="#">Registreer</a></li>
+						</ul>
+					</div>
+				</li>
+			{/if}
+		</ul>
+	</nav>
+	<!-- {/if} -->
+	<button class="menu-button" on:click={() => (active = !active)}>
+		<svg
+			width="2em"
+			height="2em"
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			stroke="#fff"
+		>
+			<g id="SVGRepo_bgCarrier" stroke-width="0" />
 
-            <g id="SVGRepo_bgCarrier" stroke-width="0"/>
-            
-            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
-            
-            <g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M4 5C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H4ZM7 12C7 11.4477 7.44772 11 8 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H8C7.44772 13 7 12.5523 7 12ZM13 18C13 17.4477 13.4477 17 14 17H20C20.5523 17 21 17.4477 21 18C21 18.5523 20.5523 19 20 19H14C13.4477 19 13 18.5523 13 18Z" fill="#fff"/> </g>
-            
-            </svg>
-        </button>
-    </header>
+			<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+
+			<g id="SVGRepo_iconCarrier">
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M4 5C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H4ZM7 12C7 11.4477 7.44772 11 8 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H8C7.44772 13 7 12.5523 7 12ZM13 18C13 17.4477 13.4477 17 14 17H20C20.5523 17 21 17.4477 21 18C21 18.5523 20.5523 19 20 19H14C13.4477 19 13 18.5523 13 18Z"
+					fill="#fff"
+				/>
+			</g>
+		</svg>
+	</button>
+</header>
 
 <style>
-    header {
-    display: flex;
-    justify-content: space-between;
-    background: black;
-    align-items: center;
-    min-height: 5em;
-    padding: 0 1em;
-    position: relative;
-    z-index: 4;
-}
+	header {
+		display: flex;
+		justify-content: space-between;
+		background: black;
+		align-items: center;
+		min-height: 5em;
+		padding: 0 1em;
+		position: relative;
+		z-index: 4;
+	}
 
-header button.menu-hider{
-    display:none;
-}
+	header button.menu-hider {
+		display: none;
+	}
 
-.menu-button{
-    display:none
-}
+	.menu-button {
+		display: none;
+	}
 
-header nav>ul {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 2em;
-    flex-wrap: wrap;
-}
+	header nav > ul {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 2em;
+		flex-wrap: wrap;
+	}
 
-header nav ul p,
-header a {
-    color: white;
-}
+	header nav ul p,
+	header a {
+		color: white;
+	}
 
-header nav ul li>div {
-    display: none;
-}
+	header nav ul li > div {
+		display: none;
+	}
 
-header nav>ul>li {
-    display: flex;
-    position: relative;
-    height: 5em;
-    align-items: center;
-    gap: .5em;
-    cursor:pointer;
-}
+	header nav > ul > li {
+		display: flex;
+		position: relative;
+		height: 5em;
+		align-items: center;
+		gap: 0.5em;
+		cursor: pointer;
+	}
 
-header nav ul>li>div>ul {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-}
+	header nav ul > li > div > ul {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+	}
 
-header nav ul>li>div>ul>li {
-    height: 2em;
-    display: flex;
-    align-items: center;
-    width: 100%;
-}
+	header nav ul > li > div > ul > li {
+		height: 2em;
+		display: flex;
+		align-items: center;
+		width: 100%;
+	}
 
-header nav ul>li>div>ul>li>a {
-    width: 100%;
-}
+	header nav ul > li > div > ul > li > a {
+		width: 100%;
+	}
 
-header nav>ul>li img {
-    width: 2em;
-    height: 2em;
-    border-radius: 15em;
-    background: yellow;
-}
+	header nav > ul > li img {
+		width: 2em;
+		height: 2em;
+		border-radius: 15em;
+		background: yellow;
+	}
 
-header a img {
-    /* width: 2em; */
-    height: 2em;
-}
+	header a img {
+		/* width: 2em; */
+		height: 2em;
+	}
 
-header a img {
-    /* width: 2em; */
-    height: 2em;
-}
+	header a img {
+		/* width: 2em; */
+		height: 2em;
+	}
 
-.menu-hider {
-    all: unset;
-    position: absolute;
-    height: 100vh;
-    width: 100vw;
-    top: 0;
-    left: 0;
-    background: rgb(0 0 0 /.5);
-    display:none;
-    backdrop-filter: blur(.1em);
-}
+	.menu-hider {
+		all: unset;
+		position: absolute;
+		height: 100vh;
+		width: 100vw;
+		top: 0;
+		left: 0;
+		background: rgb(0 0 0 /0.5);
+		display: none;
+		backdrop-filter: blur(0.1em);
+	}
 
-@media (max-width:866px) {
-    header nav{
-        display:none;
-    }
+	@media (max-width: 866px) {
+		header nav {
+			display: none;
+		}
 
-    .menu-button{
-        display: flex;
-    }
-    header.active nav {
-        position: absolute;
-        top: 0;
-        right: 0;
-        height: 100vh;
-        width: 15em;
-        background: black;
-        padding: 1em;
-        display:flex;
-        border-left:.2em solid yellow;
-        flex-direction: row;
-    }
+		.menu-button {
+			display: flex;
+		}
+		header.active nav {
+			position: absolute;
+			top: 0;
+			right: 0;
+			height: 100vh;
+			width: 15em;
+			background: black;
+			padding: 1em;
+			display: flex;
+			border-left: 0.2em solid yellow;
+			flex-direction: row;
+		}
 
-    header.active > nav > ul{
-        flex-direction: row!important;;
-    }
+		header.active > nav > ul {
+			flex-direction: row !important;
+		}
 
-    header.active > nav > ul {
-        display: flex;
-        gap: 0em;
-        flex-direction: column;
-        height: 100%;
-        overflow-y:scroll;
-    }
+		header.active > nav > ul {
+			display: flex;
+			gap: 0em;
+			flex-direction: column;
+			height: 100%;
+			overflow-y: scroll;
+		}
 
-    header.active > nav > ul::-webkit-scrollbar{
-        display:none;
-    }
+		header.active > nav > ul::-webkit-scrollbar {
+			display: none;
+		}
 
-    header.active nav ul li {
-        width: 100%;
-        height:unset;
-        min-height:3em;
-    }
+		header.active nav ul li {
+			width: 100%;
+			height: unset;
+			min-height: 3em;
+		}
 
-    header.active nav > ul > li > a,
-    header.active nav > ul > li > p {
-        width: 100%;
-        text-align: center;
-        color: #fff;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+		header.active nav > ul > li > a,
+		header.active nav > ul > li > p {
+			width: 100%;
+			text-align: center;
+			color: #fff;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
 
-    header.active nav ul li:last-of-type {
-        display: flex;
-        flex-wrap: wrap;
-        position: relative;
-        margin-top: auto;
-        background: white;
-        padding: 1em;
-        border-radius: .5em;
-    }
+		header.active nav ul li:last-of-type {
+			display: flex;
+			flex-wrap: wrap;
+			position: relative;
+			margin-top: auto;
+			background: white;
+			padding: 1em;
+			border-radius: 0.5em;
+		}
 
-    header.active nav ul li:last-of-type>div {
-        display: flex;
-        flex: 0 0 100%;
-    }
+		header.active nav ul li:last-of-type > div {
+			display: flex;
+			flex: 0 0 100%;
+		}
 
-    header.active nav ul li:last-of-type>div {
-        overflow:unset;
-    }
+		header.active nav ul li:last-of-type > div {
+			overflow: unset;
+		}
 
-    header.active nav ul li:last-of-type p {
-        all: unset;
-        text-align: center;
-        display: inline-block;
-        flex: 1 1 6em;
-        margin-left: -2em;
-    }
+		header.active nav ul li:last-of-type p {
+			all: unset;
+			text-align: center;
+			display: inline-block;
+			flex: 1 1 6em;
+			margin-left: -2em;
+		}
 
-    header.active nav>ul>li img {
-        flex: 0 0 2em;
-    }
+		header.active nav > ul > li img {
+			flex: 0 0 2em;
+		}
 
-    header.active nav ul>li>div>ul>li a {
-        color: black;
-        text-align: center;
-    }
+		header.active nav ul > li > div > ul > li a {
+			color: black;
+			text-align: center;
+		}
 
-    header.active nav ul>li>div>ul>li>a {
-        position: relative;
-    }
+		header.active nav ul > li > div > ul > li > a {
+			position: relative;
+		}
 
-    header.active nav>ul>li:first-of-type {
-        border-radius: .5em .5em 0 0;
-    }
+		header.active nav > ul > li:first-of-type {
+			border-radius: 0.5em 0.5em 0 0;
+		}
 
-    header.active nav>ul>li:nth-of-type(6) {
-        border-radius: 0 0 .5em .5em;
-    }
+		header.active nav > ul > li:nth-of-type(6) {
+			border-radius: 0 0 0.5em 0.5em;
+		}
 
-    .menu-button {
-      display: block;
-    }
-    header.active .menu-hider {
-      display: flex;
-    }
-    li.open {
-    display: flex;
-    position: relative;
-    flex-wrap:wrap;
-    background:white;
-    border-radius: .2em;
-    padding:1em;
-    overflow:hidden;
-}
+		.menu-button {
+			display: block;
+		}
+		header.active .menu-hider {
+			display: flex;
+		}
+		li.open {
+			display: flex;
+			position: relative;
+			flex-wrap: wrap;
+			background: white;
+			border-radius: 0.2em;
+			padding: 1em;
+			overflow: hidden;
+		}
 
-nav > ul > li::after{
-    content:'^';
-    position:absolute;
-    top: 1em;
-    right:1em;
-    transform:rotate(180deg);
-    color:black;
-}
-li.open div{
-    display:flex;
-    flex-wrap:wrap;
-    width:100%;
-}
-li.open div a{
-    width:100%;
-    text-align:center;
-    padding-top:.5em;
-}
+		nav > ul > li::after {
+			content: '^';
+			position: absolute;
+			top: 1em;
+			right: 1em;
+			transform: rotate(180deg);
+			color: black;
+		}
+		li.open div {
+			display: flex;
+			flex-wrap: wrap;
+			width: 100%;
+		}
+		li.open div a {
+			width: 100%;
+			text-align: center;
+			padding-top: 0.5em;
+		}
 
-li.open div a:first-of-type{
-    border-top:.1em solid black;
-}
+		li.open div a:first-of-type {
+			border-top: 0.1em solid black;
+		}
 
-li.open > p, li.open a{
-    color:black!important;
-}
-}
+		li.open > p,
+		li.open a {
+			color: black !important;
+		}
+	}
 
-@media screen and (min-width:866px){
-    header nav ul li:hover>div,
-header nav ul li:hover>div:hover {
-    display: flex;
-    top: 4em;
-    left: 0;
-    position: absolute;
-    background: currentColor;
-    padding: 1em;
-    gap: 1em;
-    flex-direction: column;
-    width: max-content;
-    box-shadow: 0 0.4rem 1.2rem rgba(0, 0, 0, .25);
-}
+	@media screen and (min-width: 866px) {
+		header nav ul li:hover > div,
+		header nav ul li:hover > div:hover {
+			display: flex;
+			top: 4em;
+			left: 0;
+			position: absolute;
+			background: currentColor;
+			padding: 1em;
+			gap: 1em;
+			flex-direction: column;
+			width: max-content;
+			box-shadow: 0 0.4rem 1.2rem rgba(0, 0, 0, 0.25);
+		}
 
-header nav ul p:hover, header a:hover {
-    color: yellow;
-}
-}
+		header nav ul p:hover,
+		header a:hover {
+			color: yellow;
+		}
+	}
 </style>
--- src/routes/challenger/profiel/components/Challenges.svelte
+++ src/routes/challenger/profiel/components/Challenges.svelte
@@ -1,56 +1,67 @@
 <script lang="ts">
-    import ChallengeBlock from "./ChallengeBlock.svelte";
+	import ChallengeBlock from './ChallengeBlock.svelte';
 
-    export let username: string;
-    let challengeName: string;
+	export let username: string;
+	let challengeName: string;
 </script>
 
 <section>
-    <h2>My challenges</h2>
-    <a href="/challenge/create">Add challenge</a>
+	<h2>My challenges</h2>
+	<a href="/challenge/create">Add challenge</a>
 
-    <ul>
-        <ChallengeBlock challengeName = {challengeName} username = {username}/>
+	<ul>
+		<ChallengeBlock {challengeName} {username} />
 	</ul>
 </section>
 
 <style lang="css">
+	section {
+		padding: 1em;
 
-    section{
-        /* background-color: lightblue; */
-        padding: 1em;
-        border-left: solid .1em var(--gray-font);
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		grid-template-rows: 4em auto;
+	}
 
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: 4em auto;
-    }
+	@media (min-width: 1200px) {
+		section {
+			border-left: solid 0.1em var(--gray-font);
+		}
+	}
 
-    h2{
-        width: 100%;
-        height: fit-content;
-        font-size: 2em;
-        font-family: var(--optima);
-        font-weight: bold;
+	h2 {
+		width: 100%;
+		height: fit-content;
+		font-size: clamp(1em , 1.3em , 1.5em );
+		font-family: var(--optima);
+		font-weight: bold;
 
-        align-self: center;
-    }
+		align-self: center;
+	}
 
-    section > a{
-        width: fit-content;
-        height: fit-content;
-        padding: var(--padding-button);
-        background-color: var(--theme-color);
-        align-self: center;
-        justify-self: end;
-    }
+	@media (min-width: 1200px) {
+		h2 {
+			font-size: 2em;
+		}
+	}
 
-    ul{
-        grid-column: 1/-1;
+	section > a {
+		width: fit-content;
+		height: fit-content;
+		padding: var(--padding-button);
+		background-color: var(--theme-color);
+		align-self: center;
+		justify-self: end;
 
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        gap: 1em;
-    }
+        font-size: clamp(.8em , .8em , 1.5em);
+	}
+
+	ul {
+		grid-column: 1/-1;
+
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 1em;
+	}
 </style>
