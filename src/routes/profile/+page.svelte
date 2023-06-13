190a752cd486fb6e7220eb891a34dcb5dd608ad0
GekkeBoyJeff <jeffrey.ullers@hva.nl>
2023-06-13 14:15:34 +0200

    start of page

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+
+<main>
+    <section> <!-- order -1 geven. -->
+        <h1>Portfolio</h1> 
+    </section>
+    <aside>
+        <img src="" alt="">
+        <button>Remove image</button> <!-- Remove image uitwerken -->
+        <h2>username</h2>
+        <ul>
+            <li>
+                <p>Followers</p>
+                <p>0</p>
+            </li>
+            <li>
+                <p>Following</p>
+                <p>0</p>
+            </li>
+            <li>
+                <p>Project views</p>
+                <p>0</p>
+            </li>
+        </ul>
+        <nav>
+            <a href="">Portfolio</a>
+            <a href="">Profile</a>
+            <a href="">Settings</a>
+            <a href=""></a>
+            <a href="">Sign out</a>
+        </nav>
+    </aside>
+
+</main>
+
+<style>
+
+</style>
