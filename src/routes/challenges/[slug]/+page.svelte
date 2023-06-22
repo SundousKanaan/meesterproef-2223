cacb8882d90be9a0ffef360151746714b1241784
Bram-ter <bram.tersteeg2@hva.nl>
2023-06-22 16:30:35 +0200

    add participants component

--- src/routes/challenges/[slug]/+page.svelte
+++ src/routes/challenges/[slug]/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Participants from '$lib/Participants.svelte';
+    import Timeline from '$lib/Timeline.svelte';
 	import ChallengeInfo from '$lib/challengeInfo.svelte';
     import CtaSection from '$lib/ctaSection.svelte';
     import HeroSlider from '$lib/heroSlider.svelte';
@@ -9,9 +11,19 @@
 
 <ChallengeInfo />
 
+<section>
+    <Timeline />
+</section>
+
 <CtaSection />
 
 <PrizesSection />
 
+<Participants />
+
 <style>
+    section {
+        margin: 0 auto;
+        width: 80%;
+    }
 </style>
