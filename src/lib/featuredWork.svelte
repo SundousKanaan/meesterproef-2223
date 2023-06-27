223e005649bf15e5300fccbc8ba6b4691d3dd271
GekkeBoyJeff <jeffrey.ullers@hva.nl>
2023-06-27 10:08:04 +0200

    Made a lot of components. FIxed a lot of broken css responsive issues. added loadMore functionality.  Added a lot of data via an array

---
+++ src/lib/featuredWork.svelte
@@ -0,0 +1,39 @@
+
+<script>
+    const data = [
+      {image: "/shoe.webp", challenge: "Schoen tekenen", creator: "John", link: "/challenges/schoen-tekenen"},
+      {image: "/eye.png", challenge: "Ogen tekenen", creator: "Jane", link: "/challenges/ogen-tekenen"},
+      {image: "/beans.png", challenge: "Bonen tekenen", creator: "Joan", link: "/challenges/bonen-tekenen"},
+      {image: "/shoe.webp", challenge: "Schoen schilderen", creator: "James", link: "/challenges/schoen-schilderen"},
+      {image: "/eye.png", challenge: "Ogen schetsen", creator: "Jill", link: "/challenges/ogen-schetsen"},
+      {image: "/beans.png", challenge: "Bonen beeldhouwen", creator: "Jerry", link: "/challenges/bonen-beeldhouwen"},
+      {image: "/shoe.webp", challenge: "Schoen ontwerpen", creator: "Jenny", link: "/challenges/schoen-ontwerpen"},
+      {image: "/eye.png", challenge: "Ogen fotograferen", creator: "Jack", link: "/challenges/ogen-fotograferen"},
+      {image: "/beans.png", challenge: "Bonen printen", creator: "Jacob", link: "/challenges/bonen-printen"},
+      {image: "/shoe.webp", challenge: "Schoen animeren", creator: "Jasmine", link: "/challenges/schoen-animeren"},
+      {image: "/eye.png", challenge: "Ogen digitaliseren", creator: "Jaden", link: "/challenges/ogen-digitaliseren"},
+      {image: "/beans.png", challenge: "Bonen grafisch ontwerpen", creator: "Julia", link: "/challenges/bonen-grafisch-ontwerpen"},
+      {image: "/shoe.webp", challenge: "Schoen beeldhouwen", creator: "Jordan", link: "/challenges/schoen-beeldhouwen"},
+      {image: "/eye.png", challenge: "Ogen illustreren", creator: "Jenna", link: "/challenges/ogen-illustreren"},
+      {image: "/beans.png", challenge: "Bonen tekenen", creator: "Joe", link: "/challenges/bonen-tekenen-2"},
+      {image: "/shoe.webp", challenge: "Schoen schilderen", creator: "Janet", link: "/challenges/schoen-schilderen-2"},
+      {image: "/eye.png", challenge: "Ogen schetsen", creator: "Jeff", link: "/challenges/ogen-schetsen-2"},
+      {image: "/beans.png", challenge: "Bonen beeldhouwen", creator: "Josie", link: "/challenges/bonen-beeldhouwen-2"},
+    ];
+  </script>
+
+<section>
+    <h2>Featured work</h2>
+    <ul>
+        <!-- sveltekit each item in array becomes a listitem -->
+        {#each data as item}
+        <li>
+            <a href={item.link}>
+                <img src={item.image} alt={item.challenge} />
+                <h3>{item.challenge}</h3>
+                <p>by {item.creator}</p>
+            </a>
+        </li>
+        {/each}
+    </ul>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,22 @@
     import Hero from '$lib/Hero.svelte';
     import SubmittedChallenges from '$lib/submittedChallenges.svelte';
     import FeaturedCreators from '$lib/featuredCreators.svelte';
+    import FeaturedWork from '$lib/featuredWork.svelte';
 </script>
 
 <Hero />
-<SubmittedChallenges />
-<FeaturedCreators />
+
+<main>
+    <FeaturedWork />
+    <FeaturedCreators />
+</main>
 
 <!-- <DragenDropp /> -->
 
 <style>
-
+    main{
+        max-width:75em;
+        margin: 0 auto;
+        padding: 0em 2em 1em 1em;
+    }
 </style>
